a779ad115e6644b222765331c0c305e1b48cc776
Humberto <jose_skate75@hotmail.com>
2017-10-23 00:12:14 -0500

    -The controller of gallery was implemented - The view of gallery was implemented - Contact Us modal was modified in footer related with the buttons - The functionality of close icon was added

--- modal.js
+++ modal.js
@@ -1,35 +1,30 @@
-angular.module('ui.bootstrap.demo', ['ngAnimate', 'ui.bootstrap']);
+angular.module('demopage', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 
-// YOUR CONTROLLER
-angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', CarouselDemoCtrl);
-CarouselDemoCtrl.$inject = ['$scope', '$uibModal', '$log'];
-function CarouselDemoCtrl($scope, $uibModal, $log) {
+// ModalController
+angular.module('demopage').controller('ModalCtrl', ModalCtrl);
+ModalCtrl.$inject = ['$scope', '$uibModal', '$log'];
+function ModalCtrl($scope, $uibModal, $log) {
 
     $scope.open = function (size) {
 
         var modalInstance = $uibModal.open({
             animation: true,
             templateUrl: 'myModalContent.html',
-            controller: 'ModalInstanceCtrl',  // The modal controller name ('ModalInstanceCtrl')
+            controller: 'ModalInstanceCtrl', 
             size: size,
             resolve: {
                 errormessage: function () {
-                    console.log('lllllllllllll')
-                    return "Some error occured";
+                    console.log('Contact Us is not available in this moment');
+                    return "modal opening failed";
                 }
             }
         });
 
-        modalInstance.result.then(function (selectedItem) {
-            $scope.selected = selectedItem;
-        }, function () {
-            $log.info('Modal dismissed at: ' + new Date());
-        });
     };
 }
 
-// REGISTER THE MODAL CONTROLLER HERE
-angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', ModalInstanceCtrl);
+//Buttons of Modal Events
+angular.module('demopage').controller('ModalInstanceCtrl', ModalInstanceCtrl);
 ModalInstanceCtrl.$inject = ['$scope', '$uibModalInstance'];
 function ModalInstanceCtrl($scope, $uibModalInstance) {
 
@@ -42,4 +37,54 @@ function ModalInstanceCtrl($scope, $uibModalInstance) {
     $scope.cancel = function () {
         $uibModalInstance.dismiss('cancel');
     };
+}
+
+//Controller used to insert the carousel
+angular.module('demopage').controller('Carousel',CarouselCtrl); 
+CarouselCtrl.$inject = ['$scope', '$window'];
+function CarouselCtrl ($scope, $window) {
+    $scope.myInterval = 2000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var index = 0;
+
+  //Function used to insert the images 
+  $scope.addSlide = function () {
+        var img = "grandcanyon.jpg";
+        var img2 = "grandcanyon2.jpg";
+        var img3 = "grandcanyon3.jpg";
+        slides.push({
+            image: img,
+            text: 'GrandCanyon 1',
+            id: index++
+        }, 
+        {
+            image: img2,
+            text: 'GrandCanyon 2',
+            id: index++
+        },
+        {
+            image: img3,
+            text: 'GrandCanyon 3',
+            id: index++
+        },
+    );
+  };
+
+
+  $scope.addSlide();
+
+  //Function used to redirect using the id of the seleted image of the caorusel
+  $scope.redirect = function (id) {
+      if(id == 0) {
+          $window.location.href = 'http://www.google.com';
+      }
+      else if (id == 1) {
+          $window.location.href = 'http://www.yahoo.com';
+      }else if(id == 2){
+          $window.location.href = 'http://www.bing.com';
+      }
+  }
+
 }
